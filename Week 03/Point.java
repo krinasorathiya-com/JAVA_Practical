76d25da341d95909c0fccc5f498027e74154f233
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-07-22 13:20:31 +0530

    fix: add missing newline at end of Point class

--- Week 03/Point.java
+++ Week 03/Point.java
@@ -1,4 +1,28 @@
-public class Point{
-    private int x,y;
+class Point {
+    private int x, y;
+
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
     
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
+
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (!(obj instanceof Point))
+            return false;
+
+        Point p = (Point) obj;
+        return x == p.x && y == p.y;
+    }
+
+
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
 }
