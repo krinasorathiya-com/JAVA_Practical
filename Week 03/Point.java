97edcbf71037d6fb5ea3c78e627c89310a39c7c7
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-07-22 12:39:49 +0530

    refactor: update Point class structure and formatting

--- Week 03/Point.java
+++ Week 03/Point.java
@@ -1,3 +1,17 @@
-public class Point {
-    
+class Point {
+    int x, y;
+
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
+
+    public static void main(String[] args) {
+        Point p = new Point(5, 10);
+        System.out.println(p);
+    }
 }
