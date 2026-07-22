ffdfcd69eedd1e4b203c147d021c08a2db70f3d2
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-07-22 12:49:21 +0530

    refactor: remove constructor and override methods from Point class

--- Week 03/Point.java
+++ Week 03/Point.java
@@ -1,31 +1,4 @@
-import java.util.Objects;
-
-class Point {
-    private int x, y;
-
-    // Constructor
-    Point(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-
-
-    public String toString() {
-        return "(" + x + ", " + y + ")";
-    }
-
+public class Point{
+    private int x,y;
     
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (!(obj instanceof Point))
-            return false;
-
-        Point p = (Point) obj;
-        return x == p.x && y == p.y;
-    }
-
-    public int hashCode() {
-        return Objects.hash(x, y);
-    }
 }
