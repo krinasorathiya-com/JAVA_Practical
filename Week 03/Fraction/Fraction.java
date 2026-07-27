de93a235e18509a73148d1532623ded647b2f4f3
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-07-27 22:26:25 +0530

    feat: implement Fraction class with arithmetic operations and overrides for toString, equals, and hashCode

---
+++ Week 03/Fraction/Fraction.java
@@ -0,0 +1,52 @@
+package Week 03.Fraction;
+
+public import java.util.Objects;
+
+public class Fraction {
+
+    private final int num;
+    private final int den;
+
+    public Fraction(int num, int den) {
+
+        int g = gcd(num, den);
+
+        this.num = num / g;
+        this.den = den / g;
+    }
+
+    private int gcd(int a, int b) {
+
+        while (b != 0) {
+            int t = b;
+            b = a % b;
+            a = t;
+        }
+
+        return a;
+    }
+
+    @Override
+    public String toString() {
+        return num + "/" + den;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+
+        if (this == obj)
+            return true;
+
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+
+        Fraction f = (Fraction) obj;
+
+        return num == f.num && den == f.den;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(num, den);
+    }
+}
