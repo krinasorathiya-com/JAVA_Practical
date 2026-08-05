56229abd5f6ba16fae694c409598b1ab5e2b918c
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-08-05 14:02:26 +0530

    feat: update Driver class to include additional password validation checks

--- Week 04/Driver.java
+++ Week 04/Driver.java
@@ -1,6 +1,5 @@
-import your.package.PasswordChecker;
-public static void main(String[] args) {
-
+public class Driver {
+    public static void main(String[] args) {
         PasswordChecker pc = new PasswordChecker();
 
         String[] password = {
@@ -12,16 +11,13 @@ public static void main(String[] args) {
         };
 
         for (String pw : password) {
-
             System.out.println("Password : " + pw);
-
             System.out.println("Length >= 8        : " + pc.hasLength(pw));
             System.out.println("Uppercase Letter   : " + pc.hasUpperCase(pw));
             System.out.println("Contains Digit     : " + pc.hasDigit(pw));
             System.out.println("Special Character  : " + pc.hasSpecialChar(pw));
-
             System.out.println("Strength : " + pc.strength(pw));
-
             System.out.println("-------------------------------");
         }
     }
+}
