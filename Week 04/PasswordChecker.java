b69054d87e1b74bfc30eb77c83489772ff24642f
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-08-05 13:43:48 +0530

    feat: implement password strength validation in PasswordChecker class

--- Week 04/PasswordChecker.java
+++ Week 04/PasswordChecker.java
@@ -1,5 +1,13 @@
-package Week 04;
-
 public class PasswordChecker {
-    
+
+    public String strength(String password) {
+
+        String regex = "^(?=.*[A-Z])(?=.*\\d)(?=.*[^a-zA-Z0-9]).{8,}$";
+
+        if (password.matches(regex)) {
+            return "Strong";
+        } else {
+            return "Weak/Medium";
+        }
+    }
 }
