565bdabca970e0d74b319f9d70d2e8e1627fd30e
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-08-05 13:58:33 +0530

    feat: add main method to Driver class for password validation demonstration

---
+++ Week 04/Driver.java
@@ -0,0 +1,27 @@
+public static void main(String[] args) {
+
+        PasswordChecker pc = new PasswordChecker();
+
+        String[] password = {
+                "abc",
+                "Password",
+                "Pass123",
+                "Abcd1234!",
+                "HELLO@12"
+        };
+
+        for (String pw : password) {
+
+            System.out.println("Password : " + pw);
+
+            System.out.println("Length >= 8        : " + pc.hasLength(pw));
+            System.out.println("Uppercase Letter   : " + pc.hasUpperCase(pw));
+            System.out.println("Contains Digit     : " + pc.hasDigit(pw));
+            System.out.println("Special Character  : " + pc.hasSpecialChar(pw));
+
+            System.out.println("Strength : " + pc.strength(pw));
+
+            System.out.println("-------------------------------");
+        }
+    }
+}
