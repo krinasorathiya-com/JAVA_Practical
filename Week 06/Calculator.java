9441169aaac81adce1ef43bc951d192f60264330
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-08-19 13:52:02 +0530

    feat: refactor Calculator to use try-with-resources for Scanner and improve exception handling

--- Week 06/Calculator.java
+++ Week 06/Calculator.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 class DivideByZeroException extends Exception {
@@ -15,67 +16,51 @@ class InvalidNumberException extends Exception {
 public class Calculator {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        boolean done = false;
-
-        while (!done) {
-            try {
-                System.out.print("Enter Operand and Operator: ");
-
-                int a = sc.nextInt();
-                char op = sc.next().charAt(0);
-                int b = sc.nextInt();
-
-                if (a < 0 || b < 0)
-                    throw new InvalidNumberException("Negative number is not allowed!");
-
-                if (op == '*' && (a == 0 || b == 0))
-                    throw new InvalidNumberException("0 is not allowed in multiplication!");
-
-                if (op == '/' && b == 0)
-                    throw new DivideByZeroException("Cannot divide by zero!");
-
-                switch (op) {
-                    case '+':
-                        System.out.println("Result = " + (a + b));
-                        break;
-                    case '-':
-                        System.out.println("Result = " + (a - b));
-                        break;
-                    case '*':
-                        System.out.println("Result = " + (a * b));
-                        break;
-                    case '/':
-                        System.out.println("Result = " + (a / b));
-                        break;
-                    default:
-                        System.out.println("Invalid operator!");
-                        break;
+        try (Scanner sc = new Scanner(System.in)) {
+            boolean done = false;
+            
+            while (!done) {
+                try {
+                    System.out.print("Enter Operand and Operator: ");
+                    
+                    int a = sc.nextInt();
+                    char op = sc.next().charAt(0);
+                    int b = sc.nextInt();
+                    
+                    if (a < 0 || b < 0)
+                        throw new InvalidNumberException("Negative number is not allowed!");
+                    
+                    if (op == '*' && (a == 0 || b == 0))
+                        throw new InvalidNumberException("0 is not allowed in multiplication!");
+                    
+                    if (op == '/' && b == 0)
+                        throw new DivideByZeroException("Cannot divide by zero!");
+                    
+                    switch (op) {
+                        case '+' -> System.out.println("Result = " + (a + b));
+                        case '-' -> System.out.println("Result = " + (a - b));
+                        case '*' -> System.out.println("Result = " + (a * b));
+                        case '/' -> System.out.println("Result = " + (a / b));
+                        default -> System.out.println("Invalid operator!");
+                    }
+                    
+                    done = true;
                 }
-
-                done = true;
-            }
-
-            catch (InvalidNumberException e) {
-                System.out.println(e.getMessage());
-                sc.nextLine();
-            }
-
-            catch (DivideByZeroException e) {
-                System.out.println(e.getMessage());
-                sc.nextLine();
-            }
-
-            catch (Exception e) {
-                System.out.println("Please enter valid numbers!");
-                sc.nextLine();
-            }
-
-            finally {
-                System.out.println("Attempt logged.\n");
+                
+                catch (InvalidNumberException | DivideByZeroException e) {
+                    System.out.println(e.getMessage());
+                    sc.nextLine();
+                }
+                
+                catch (InputMismatchException e) {
+                    System.out.println("Please enter valid numbers!");
+                    sc.nextLine();
+                }
+                
+                finally {
+                    System.out.println("Attempt logged.\n");
+                }
             }
         }
-
-        sc.close();
     }
 }
