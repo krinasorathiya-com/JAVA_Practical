5e9824069ac922894c44a72c849cd8750cc88412
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-09-17 14:38:05 +0530

    feat: implement Switchable interface and add fan and Light classes with toggle functionality

--- Week 07/RemoteControl.java
+++ Week 07/RemoteControl.java
@@ -1,3 +1,50 @@
-public class RemoteControl {
-    
+interface Switchable{
+        void on();
+        void off();
+        default void toggle()
+        {
+            on();
+        }
+    }
+
+class fan implements Switchable{
+        public void on()
+        {
+            System.out.println("Fan is on.");
+        }
+        public void off()
+        {
+            System.out.println("Fan is off");
+        }
+}
+
+class Light implements Switchable{
+    public void on(){
+        System.out.println("Light is on.");
+    }
+    public void off(){
+        System.out.println("Light is off.");
+    }
+}
+
+@FunctionalInterface 
+interface permission
+{
+   boolean MyChoice(string device,int hours);
+}
+
+class RemoteControl{
+    public static void main(String[] args)
+    {
+        Switchable[] devices = {new fan(), new Light()};
+    for (Switchable device : devices)
+    {
+        device.toggle();
+    }
+    permission p1=new permission() {
+        boolean MyChoice(string device,int hours){
+            return hour>6;
+        }
+    };
+}
 }
