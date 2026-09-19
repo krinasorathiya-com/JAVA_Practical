f7d758fa1197c33a180b7770e7f732243dc59a61
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-09-19 20:51:23 +0530

    feat: add Urgent and UrgentNotifier classes with functional interface for message handling

--- Week 07/NotificationDemo.java
+++ Week 07/NotificationDemo.java
@@ -3,11 +3,9 @@ interface Notifier {
     void send(String message);
 }
 
-// Marker interface
 interface Urgent {
 }
 
-// Wrapper class for urgent senders
 class UrgentNotifier implements Notifier, Urgent {
     private Notifier notifier;
 
@@ -24,14 +22,12 @@ class UrgentNotifier implements Notifier, Urgent {
 public class NotificationDemo {
     public static void main(String[] args) {
 
-        // Email and SMS senders using lambda expressions
         Notifier email = message ->
                 System.out.println("Email: " + message);
 
         Notifier sms = message ->
                 System.out.println("SMS: " + message);
 
-        // Mark the SMS sender as urgent
         Notifier urgentSms = new UrgentNotifier(sms);
 
         Notifier[] senders = {email, urgentSms};
@@ -43,7 +39,6 @@ public class NotificationDemo {
         for (Notifier sender : senders) {
             sender.send(message);
 
-            // Send again if the sender is urgent
             if (sender instanceof Urgent) {
                 sender.send(message);
             }
